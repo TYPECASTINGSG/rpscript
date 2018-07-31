c1c52ec3d981e8fa2138fa7f2ab9cc129a266760
James Chong <wei3hua2@gmail.com>
2018-07-31 09:30:52 +0800

    added install uninstall hook

---
+++ scripts/install.js
@@ -0,0 +1,9 @@
+const { exec } = require('child_process');
+exec('rps install basic ramda', (err, stdout, stderr) => {
+  if (err) {
+    console.log('Fail to install basic modules : ');
+    console.error(stderr);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
